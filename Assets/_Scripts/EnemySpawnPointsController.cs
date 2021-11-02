79a37ca95ca97889b66975395159848a0972f819
Артём Волков <lobachevartem@gmail.com>
2021-11-02 21:44:41 +0300

    Спавн поинты

---
+++ Assets/_Scripts/EnemySpawnPointsController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+	public class EnemySpawnPointsController : MonoBehaviour
+	{
+		[SerializeField] private SpawnPoint[] _enemySpawnPoints;
+		[SerializeField] private GameObject _enemyPrefab;
+
+		public void Spawn(int spawnPointIndex)
+		{
+			_enemySpawnPoints[spawnPointIndex].Spawn(_enemyPrefab);
+		}
+	}
+}
---
+++ Assets/_Scripts/SpawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+	public class SpawnPoint : MonoBehaviour
+	{
+		public T Spawn<T>(T pawn)
+		where T : Object
+		{
+			return Instantiate(pawn, transform.position, Quaternion.identity);
+		}
+	}
+}
