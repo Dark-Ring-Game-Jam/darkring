4986534593a9a75667f396396a8418ddc7be4f39
Артём Волков <lobachevartem@gmail.com>
2021-11-04 15:48:38 +0300

    Добавил компоненты атаки и аниматора

---
+++ Assets/_Scripts/Components/AttackComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Components
+{
+	public class AttackComponent : MonoBehaviour
+	{
+		[SerializeField] private float _distanceToAttack;
+		[SerializeField] private float _delayToAttack;
+		[SerializeField] private int _damage;
+
+		public float DistanceToAttack => _distanceToAttack;
+		public float DelayToAttack => _delayToAttack;
+
+		private Coroutine _currentCoroutine;
+
+		public void Attack(HealthComponent healthComponent)
+		{
+			if (_currentCoroutine == null)
+			{
+				_currentCoroutine = StartCoroutine(AttackCoroutine(healthComponent));
+			}
+		}
+
+		private IEnumerator AttackCoroutine(HealthComponent healthComponent)
+		{
+			yield return new WaitForSeconds(_delayToAttack);
+
+			healthComponent.TakeDamage(_damage);
+			Debug.LogError("Attack");
+			_currentCoroutine = null;
+		}
+	}
+}
--- Assets/_Scripts/Pawns/Enemies/Enemy.cs
+++ Assets/_Scripts/Pawns/Enemies/Enemy.cs
@@ -6,10 +6,12 @@ namespace _Scripts
 	[RequireComponent(typeof(HealthComponent), typeof(MovementComponent))]
 	public class Enemy : MonoBehaviour
 	{
+		[SerializeField] private Collider2D _collider2D;
+		[Header("Components")]
 		[SerializeField] private HealthComponent _healthComponent;
 		[SerializeField] private MovementComponent _movementComponent;
-		[SerializeField] private float _distaneToAttack;
-		[SerializeField] private int _damage;
+		[SerializeField] private AnimatorComponent _animatorComponent;
+		[SerializeField] private AttackComponent _attackComponent;
 
 		public HealthComponent HealthComponent => _healthComponent;
 		public MovementComponent MovementComponent => _movementComponent;
@@ -18,16 +20,24 @@ namespace _Scripts
 
 		private void Awake()
 		{
-			_heroHealthComponent = GameObject.FindGameObjectWithTag("hero").GetComponent<HealthComponent>();
+			//_heroHealthComponent = GameObject.FindGameObjectWithTag("hero").GetComponent<HealthComponent>();
+			_healthComponent.OnDeath += Die;
+			_animatorComponent.Init(_attackComponent.DelayToAttack);
 		}
 
 		private void Update()
 		{
-			if (Vector2.Distance(transform.position, _heroHealthComponent.transform.position) <= _distaneToAttack)
+			/*if (Vector2.Distance(transform.position, _heroHealthComponent.transform.position) <= _attackComponent.DistanceToAttack)
 			{
-				_healthComponent.TakeDamage(_damage);
-			}
+				_attackComponent.Attack(_heroHealthComponent);
+			}*/
 		}
 
+		private void Die()
+		{
+			_animatorComponent.Die();
+			_collider2D.enabled = false;
+			_healthComponent.OnDeath -= Die;
+		}
 	}
 }
