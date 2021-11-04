c3411096bc9aa2fa8e98725b141f7297134b721e
Артём Волков <lobachevartem@gmail.com>
2021-11-04 12:47:17 +0300

    WIP

--- Assets/_Scripts/Pawns/Enemies/Enemy.cs
+++ Assets/_Scripts/Pawns/Enemies/Enemy.cs
@@ -8,8 +8,26 @@ namespace _Scripts
 	{
 		[SerializeField] private HealthComponent _healthComponent;
 		[SerializeField] private MovementComponent _movementComponent;
+		[SerializeField] private float _distaneToAttack;
+		[SerializeField] private int _damage;
 
 		public HealthComponent HealthComponent => _healthComponent;
 		public MovementComponent MovementComponent => _movementComponent;
+
+		private HealthComponent _heroHealthComponent;
+
+		private void Awake()
+		{
+			_heroHealthComponent = GameObject.FindGameObjectWithTag("hero").GetComponent<HealthComponent>();
+		}
+
+		private void Update()
+		{
+			if (Vector2.Distance(transform.position, _heroHealthComponent.transform.position) <= _distaneToAttack)
+			{
+				_healthComponent.TakeDamage(_damage);
+			}
+		}
+
 	}
 }
