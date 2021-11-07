0fc3e3f979919cb05f8e9ec71bcfa6606c4df9ec
Артём Волков <lobachevartem@gmail.com>
2021-11-07 16:24:02 +0300

    Конец игры

---
+++ Assets/_Scripts/Events/EndGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace _Scripts
+{
+	public class EndGame : MonoBehaviour
+	{
+		[SerializeField] private FinalSlide _finalSlide;
+
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.TryGetComponent(out Player _))
+			{
+				_finalSlide.gameObject.SetActive(true);
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/FinalSlide.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts
+{
+	public class FinalSlide : MonoBehaviour
+	{
+		[SerializeField] private Image _image;
+		[SerializeField] private float _step;
+
+		private float _visibility;
+
+		private void Update()
+		{
+			var imageColor = _image.color;
+			_visibility = Mathf.Lerp(_visibility, 1, _step);
+			imageColor.a = _visibility;
+			_image.color = imageColor;
+		}
+	}
+}
