49004a42c7520566e3afb73acfe4a2e9a282915a
Chebotov Nikolay <spiritkola@hotmail.com>
2021-11-10 01:08:24 +0300

    update: Добавлена возможность пропустить intro

--- Assets/_Scripts/UI/ComicBook.cs
+++ Assets/_Scripts/UI/ComicBook.cs
@@ -15,30 +15,43 @@ namespace _Scripts.UI
         [field: SerializeField] private float[] delays;
 
         private int _currentSlide = 0;
+        private bool _startGame;
 
         protected void Start()
         {
             StartCoroutine(ShowNextSlide(0f));
         }
 
+        protected void Update()
+        {
+            if (!_startGame && Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Escape) || Input.GetKey(KeyCode.E) ||
+                Input.GetKey(KeyCode.Space))
+            {
+                _startGame = true;
+                StopAllCoroutines();
+                StartCoroutine(StartGame());
+            }
+        }
+
         private IEnumerator ShowNextSlide(float delay)
         {
-            yield return new WaitForSeconds(delay);
-
-            image.texture = textures[_currentSlide];
-            PlayClip(sounds[_currentSlide]);
-            _currentSlide++;
-
-            if (_currentSlide < textures.Length)
+            if (!_startGame)
             {
-                StartCoroutine(ShowNextSlide(this.delays[_currentSlide]));
-            }
-            else
-            {
-                StartCoroutine(StartGame());
+                yield return new WaitForSeconds(delay);
 
+                image.texture = textures[_currentSlide];
+                PlayClip(sounds[_currentSlide]);
+                _currentSlide++;
+
+                if (_currentSlide < textures.Length)
+                {
+                    StartCoroutine(ShowNextSlide(this.delays[_currentSlide]));
+                }
+                else
+                {
+                    StartCoroutine(StartGame());
+                }
             }
-            
         }
 
         private IEnumerator StartGame()
